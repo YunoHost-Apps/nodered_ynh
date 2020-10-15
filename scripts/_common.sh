263b580b44526295779c2e6273045874e8d76fa7
ericgaspar <junk.eg@free.fr>
2020-10-15 12:28:58 +0200

    v.1.2.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,3 +16,16 @@ nodejs_version=12
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+# Execute a command as another user
+# usage: exec_as USER COMMAND [ARG ...]
+exec_as() {
+  local USER=$1
+  shift 1
+
+  if [[ $USER = $(whoami) ]]; then
+    eval "$@"
+  else
+    sudo -u "$USER" "$@"
+  fi
+}
